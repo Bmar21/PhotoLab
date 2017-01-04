d2d0d76c8ce1ac0ac20984fd293688f6390cda8e
Bmar21 <brandonmalor21@gmail.com>
2017-01-04 13:03:54 -0700

    added getCount and getLargest

--- src/pixLab/classes/IntArrayWorker.java
+++ src/pixLab/classes/IntArrayWorker.java
@@ -12,10 +12,41 @@ public class IntArrayWorker
     matrix = theMatrix;
   }
   
-  /**
-   * Method to return the total 
-   * @return the total of the values in the array
-   */
+  public int getLargest()
+  {
+	  int largest = Integer.MIN_VALUE;
+	  
+	  for (int row = 0; row < matrix.length; row++)
+	  {
+		  for (int col = 0; col < matrix[0].length; col++)
+		  {
+			  if (matrix[row][col] > largest)
+			  {
+				  largest = matrix[row][col];
+			  }
+		  }
+	  }  
+	  return largest;
+  }
+  
+  public int getCount(int numberToFind)
+  {
+	  int count = 0;
+	  
+	  for (int [] row : matrix)
+	  {
+		  for (int item : row)
+		  {
+			 if (numberToFind == item)
+			 {
+				 count++;
+			 }
+		  }
+	  }
+	  
+	  return count;
+  }
+  
   public int getTotal()
   {
     int total = 0;
